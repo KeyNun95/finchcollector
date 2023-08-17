9ae31329ebd211b4f79a1c257ab506e8e2218c5d
KeyNun95 <k.nunez2017@gmail.com>
2023-08-16 20:24:19 -0400

    added feeding model

--- main_app/models.py
+++ main_app/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from django.urls import reverse
 
 # Create your models here.
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
+
 class Finch(models.Model):
     name = models.CharField(max_length=100)
     species = models.CharField(max_length=100)
@@ -13,3 +19,19 @@ class Finch(models.Model):
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'finch_id': self.id})
+    
+#one to many relationship
+class Feeding(models.Model):
+    date = models.DateField()
+    meal = models.CharField(
+        max_length=1,
+        choices=MEALS,
+        default=MEALS[0][0]
+    )
+    #create a cat_id FK. The FK creates the one to many relationship
+    #this on_delete is required to ensure that if A Finch is deleted, all of the relationships attached to it will also delete
+    #we are avoiding something called orphan records
+    cat = models.ForeignKey(Finch, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
